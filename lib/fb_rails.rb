831622d61f9bc3a91385464f7c97be2d9e58cf11
Matthew Higgins <developer@matthewhiggins.com>
2010-11-02 11:57:38 -0700

    move test specific method to test helper

--- lib/fb_rails.rb
+++ lib/fb_rails.rb
@@ -8,11 +8,6 @@ module FbRails
   autoload :Fb
   autoload :Mock
   autoload :Integration
-
-  def self.cookie(uid = 42, access_token = 'abc')
-    value = FbRails::Oauth.create_cookie({:uid => uid, :access_token => access_token}, FbRails::Config.secret)
-    {FbRails::Connect.cookie_name => value}
-  end
 end
 
 require 'fb_rails/railtie'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,4 +10,9 @@ require 'sample/test_controller'
 FbRails::Config.app_id = 'foo'
 FbRails::Config.secret = 'bar'
 
-
+FbRails.class_eval do
+  def self.cookie(uid = 42, access_token = 'abc')
+    value = FbRails::Oauth.create_cookie({:uid => uid, :access_token => access_token}, FbRails::Config.secret)
+    {FbRails::Connect.cookie_name => value}
+  end
+end
