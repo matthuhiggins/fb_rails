57cdd5f4c145e28e88111210051370aeadbfdf0b
Matthew Higgins <developer@matthewhiggins.com>
2010-11-01 21:15:54 -0700

    genericize cookie parsing

--- lib/fb_rails/connect.rb
+++ lib/fb_rails/connect.rb
@@ -6,6 +6,22 @@ module FbRails
       def cookie_name
         "fbs_#{FbRails::Config.app_id}"
       end
+
+      def parse_oauth2_cookie(cookie_string, secret)
+        return if cookie_string.blank?
+
+        hash = Rack::Utils::parse_query(cookie_string.gsub(/^\"|\"$/, ''))
+
+        payload = ''
+        hash.sort.each do |key, value|
+          payload += "#{key}=#{value}" if key != 'sig'
+        end
+
+        md5 = Digest::MD5.hexdigest("#{payload}#{secret}")
+        if md5 == hash['sig']
+          hash
+        end
+      end
     end
 
     extend ActiveSupport::Memoizable
@@ -16,20 +32,7 @@ module FbRails
     end
 
     def cookie
-      return if (cookie_string = cookies[self.class.cookie_name]).blank?
-
-      hash = Rack::Utils::parse_query(cookie_string.gsub(/^\"|\"$/, ''))
-      sorted_pairs = hash.sort
-  
-      payload = ''
-      sorted_pairs.each do |key, value|
-        payload += "#{key}=#{value}" if key != 'sig'
-      end
-  
-      md5 = Digest::MD5.hexdigest("#{payload}#{FbRails::Config.secret}")
-      if md5 == hash['sig']
-        hash
-      end
+      self.class.parse_oauth2_cookie(cookies[self.class.cookie_name], FbRails::Config.secret)
     end
     memoize :cookie
 
